3b09ba5b320116228241da489900cfe0b736ee75
Varinthorn <earnmaha@hotmail.com>
2025-04-07 02:17:45 +0700

    feat(about/desktop): add responsive design

--- src/components/BrownButton.jsx
+++ src/components/BrownButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BrownButton = ({ text, onClick, className = "" }) => {
   return (
     <button
-      className= {`bg-(--color-buttonBrown) px-4 py-2 rounded-lg  hover:bg-[#d8c2b0] cursor-pointer font-bold text-(--color-banner) ${className}`}
+      className= {`bg-(--color-buttonBrown) px-4 py-2 rounded-lg  hover:bg-[#d8c2b0] cursor-pointer font-bold text-black ${className}`}
       onClick={onClick}
     >
       {text}
