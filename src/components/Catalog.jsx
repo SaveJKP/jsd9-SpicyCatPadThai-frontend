0164823964545607dff9197a174e9e7b15de6df4
Jakapat Pattanasiri <jakapat_pattanasiri@hotmail.com>
2025-04-07 14:45:10 +0700

    feat(catalog) added bg blur

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,5 +1,14 @@
 export default function Catalog() {
   return (
-    <div>Catalog</div>
-  )
+    <div>
+      <div
+        className="fixed inset-0 z-10 flex items-center justify-center backdrop-blur-sm" 
+        // aria-labelledby={`${id}-label`} // ระบุ label สำหรับ Screen Reader
+      >
+      <div className="w-full   bg-box rounded-lg shadow-lg ">
+<h1></h1>
+      </div>
+      </div>
+    </div>
+  );
 }
