985fe3ae62d2fa7cab59046e57cbcd533ab5c8e0
kkoryax <orachun.wsst@gmail.com>
2025-04-09 11:10:52 +0700

    feat(home): created function showAll that can click next and previous to query id for page

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -6,21 +6,20 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
 
-export function BookCard({title, banner}) {
+export function BookCard({title, banner, author}) {
     return (
             <>
             <div>
                 <Card className="flex flex-col justify-center items-center w-50 md:w-90 bg-box
                 border-banner">
+                        <img src={banner}
+                        alt=""
+                        className="w-[100%] rounded-2xl p-4"/>
                     <CardContent>
-                        <img src={banner} alt=""
-                        className="w-[100%] rounded-2xl"/>
+                        <CardTitle className="text-lg font-bold text-text">{title}</CardTitle>
+                        <p className="text-text opacity-75 flex justify-center items-center">{author}</p>
                     </CardContent>
-                    <CardFooter>
-                        <CardTitle className="text-lg font-semibold text-text">{title}</CardTitle>
-                    </CardFooter>
                 </Card>
             </div>
             </>
--- src/components/ShowAll.jsx
+++ src/components/ShowAll.jsx
@@ -9,30 +9,71 @@ import {
 } from "@/components/ui/pagination"
 import { books } from "../data/products";
 import { BookCard } from "./BookCard";
+import { useState } from "react";
 
 export default function ShowAll() {
+  {/* set page */}
+  const itemsPerPage = 6
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(books.length / itemsPerPage)
+
+  {/* set index items to show book */}
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const currentBooks = books.slice(startIndex, endIndex)
+
+  const handleNextPage = (e) => {
+    e.preventDefault();
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = (e) => {
+    e.preventDefault();
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return <div>
           <section className="flex flex-row flex-wrap justify-center items-center p-8 gap-4">
-          {books.map((e) => (
-          <BookCard title={e.title} price={e.price} banner={e.banner} />
+          {currentBooks.map((book) => (
+          <BookCard key={book.id} title={book.title} banner={book.banner} author={book.author} />
         ))}
           </section>
 
+          {totalPages > 1 && (
           <Pagination className="p-8">
             <PaginationContent>
               <PaginationItem>
-                <PaginationPrevious href="#" className="text-text text-xl"/>
+                {/* Previous Page */}
+                <PaginationPrevious
+                  href="#"
+                  className="text-text text-xl"
+                  onClick={handlePreviousPage}
+                />
               </PaginationItem>
               <PaginationItem>
-                <PaginationLink href="#" className="text-text text-xl">1</PaginationLink>
+                <PaginationLink
+                  href="#"
+                  className="text-banner text-xl"
+                  isActive>{currentPage}
+                </PaginationLink>
               </PaginationItem>
               <PaginationItem>
                 <PaginationEllipsis className="text-text text-xl"/>
               </PaginationItem>
               <PaginationItem>
-                <PaginationNext href="#" className="text-text text-xl"/>
+                {/* Next Page */}
+                <PaginationNext
+                  href="#"
+                  className="text-text text-xl"
+                  onClick={handleNextPage}
+                />
               </PaginationItem>
             </PaginationContent>
           </Pagination>
+          )}
   </div>;
 }
