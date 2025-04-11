d4684c9d17ba57242a184491eab4a4bc4d3602b3
kkoryax <orachun.wsst@gmail.com>
2025-04-11 11:23:44 +0700

    feat(home/CardSlider): added mapping product to component and added responsive

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,59 +1,38 @@
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { books } from "../data/products";
+import { CarouselCard } from "./CarouselCard";
 
 export default function CardSlider({name}) {
-  return <div className="flex justify-end items-start w-[100%] h-[100%] px-6 py-8">
+  const relevantBooks = books;
 
-    <Carousel className="w-[100%] md:w-[95%] h-auto">
-      <h1 className="text-text mb-2 font-semibold">{name}</h1>
-
-      <CarouselContent className="ml-1 gap-4 w-70 md:w-90">
-
-        <CarouselItem className="bg-box rounded-2xl">
-          <div className="w-full flex flex-col justify-center items-center p-4 gap-2">
-            <img src="/src/assets/jsd_4.jpg" alt=""
-            className="w-[100%] rounded-2xl"/>
-            <h3 className="text-text">Generation Thailand JSD9</h3>
-          </div>
-        </CarouselItem>
-
-        <CarouselItem className="bg-box rounded-2xl">
-          <div className="w-full flex flex-col justify-center items-center p-4 gap-2">
-            <img src="/src/assets/foodstriker_4.jpg" alt=""
-            className="w-[100%] rounded-2xl"/>
-            <h3 className="text-text">Food Striker</h3>
-          </div>
-        </CarouselItem>
-
-        <CarouselItem className="bg-box rounded-2xl">
-          <div className="w-full flex flex-col justify-center items-center p-4 gap-2">
-            <img src="/src/assets/hikarilove.jpg" alt=""
-            className="w-[100%] rounded-2xl"/>
-            <h3 className="text-text">Hikari Love</h3>
-          </div>
-        </CarouselItem>
-
-        <CarouselItem className="bg-box rounded-2xl">
-          <div className="w-full flex flex-col justify-center items-center p-4 gap-2">
-            <img src="/src/assets/katanaway.jpg" alt=""
-            className="w-[100%] rounded-2xl"/>
-            <h3 className="text-text">Katanaway</h3>
-          </div>
-        </CarouselItem>
-
-        <CarouselItem className="md:basis-1/2 bg-box rounded-2xl">2</CarouselItem>
-        <CarouselItem className="md:basis-1/2 bg-box rounded-2xl">3</CarouselItem>
-        <CarouselItem className="md:basis-1/2 bg-box rounded-2xl">4</CarouselItem>
-        <CarouselItem className="md:basis-1/2 bg-box rounded-2xl">5</CarouselItem>
-        <CarouselItem className="md:basis-1/2 bg-box rounded-2xl">6</CarouselItem>
-      </CarouselContent>
-      {/* <CarouselPrevious />
-      <CarouselNext /> */}
-    </Carousel>
-  </div>
+  return  (
+    <div className="w-full px-4 md:px-8 py-8">
+        <h1 className="text-text text-2xl mb-4 font-semibold px-2">{name}</h1>
+        <Carousel
+            opts={{
+                align: "start",
+                // loop: true
+            }}
+            className="w-full"
+        >
+            <CarouselContent className="ml-6 gap-4">
+                {relevantBooks.map((book) => (
+                    <CarouselCard
+                        key={book.id}
+                        title={book.title}
+                        banner={book.banner}
+                        author={book.author}
+                    />
+                ))}
+            </CarouselContent>
+            {/* <CarouselPrevious className="absolute left-[-10px] md:left-[-20px] top-1/2 -translate-y-1/2" />
+            <CarouselNext className="absolute right-[-10px] md:right-[-20px] top-1/2 -translate-y-1/2" /> */}
+        </Carousel>
+    </div>
+  );
 }
