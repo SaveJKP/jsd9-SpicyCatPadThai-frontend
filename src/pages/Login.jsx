54c907795d6fabaf1361f27b2e75cbf566c5f5d8
kkoryax <orachun.wsst@gmail.com>
2025-04-15 21:38:04 +0700

    feat(login): added responsive to page (sm,md,lg)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,24 +6,24 @@ export default function Login() {
   <main >
     <div className="bg-greenBackground w-full h-fit">
       <div className="container__div">
-        <section className="flex flex-col md:flex-row justify-center items-center w-full min-h-screen gap-4">
-          <section className="flex flex-col justify-center items-center gap-4 w-full md:w-1/2 text-text">
+        <section className="flex flex-col lg:flex-row justify-center items-center w-full min-h-screen gap-4">
+          <section className="flex flex-col justify-center items-center gap-4 w-full lg:w-1/2 text-text">
             <img src="/src/assets/logo_katsubook.png" alt="Katsu Bookstore"
-            className="w-[50%] hidden md:flex"/>
+            className="w-[50%] hidden lg:flex"/>
             <img src="/src/assets/logo_katsubook_no-text.png" alt=""
-            className="w-[35%] flex md:hidden"/>
-            <p className="hidden md:flex w-fit">Welcome to bookstore Everything in One place</p>
+            className="w-[35%] md:w-[15%] flex lg:hidden"/>
+            <p className="hidden lg:flex w-fit">Welcome to bookstore Everything in One place</p>
           </section>
-          <section className="flex flex-col justify-center items-center gap-4 w-full md:w-1/2 text-text">
-            <h1 className="font-bold">Login</h1>
-            <p className="hidden md:flex">Welcome to bookstore Everything in One place</p>
+          <section className="flex flex-col justify-center items-center gap-4 w-full lg:w-1/2 text-text">
+            <h1 className="flex font-bold">Login</h1>
+            <p className="hidden md:flex lg:hidden">Welcome to bookstore Everything in One place</p>
             <form action="" className="flex flex-col justify-center items-center gap-4 w-full">
               <input type="email" placeholder="Email"
-              className="w-[50%] bg-white text-banner px-4 py-2 rounded-2xl"/>
+              className="w-[65%] md:w-[35%] lg:w-[50%] bg-white text-banner px-4 py-2 rounded-2xl"/>
               <input type="password" placeholder="Password"
-              className="w-[50%] bg-white text-banner px-4 py-2 rounded-2xl"/>
+              className="w-[65%] md:w-[35%] lg:w-[50%] bg-white text-banner px-4 py-2 rounded-2xl"/>
               <a href="">Forget Your password?</a>
-              <div className="flex flex-col justify-center items-center gap-4 w-[50%]">
+              <div className="flex flex-col justify-center items-center gap-4 w-[65%] md:w-[35%] lg:w-[50%]">
                 <Separator />
                 <Button className="w-full bg-buttonBrown px-4 py-2 font-semibold hover:cursor-pointer rounded-2xl">Login</Button>
                 <p>or</p>
