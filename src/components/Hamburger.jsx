278dea375f8cd25e0aad5d10e4d3c972d58fef6b
atichapre <aticha.pre@gmail.com>
2025-04-25 21:46:05 +0700

    fix: added my orders link in hamburger

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -21,14 +21,9 @@ export default function Hamburger() {
 
   return (
     <>
-      {token ? (
+      {!token ? (
         <div className="relative z-10 text-sm text-[var(--color-text)] min-[1024px]:hidden">
           <div className="bg-[var(--color-greenBackground)] pt-[5%] pb-[5%] leading-4 no-underline">
-            {/*  <input type="text"
-              className="flex justify-self-center items-center w-[75%] text-banner bg-text rounded-2xl px-4 py-2 md:hidden" placeholder="Search" /> */}
-
-            {/* search icon */}
-            {/* <Link to={"/search"}> */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="mb-[16px] justify-self-center min-[1024px]:hidden"
@@ -41,7 +36,12 @@ export default function Hamburger() {
             >
               <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
             </svg>
-            {/* </Link> */}
+            <Link
+              to={"/order"}
+              className="mx-15 mb-6 block items-center rounded-xl p-3 text-center hover:bg-[#dcd7c97c]"
+            >
+              My Orders
+            </Link>
             <Link
               to={"/logout"}
               className="mx-15 mb-6 block items-center rounded-xl bg-[var(--color-buttonBrown)] p-3 text-center hover:bg-[#dcd7c97c]"
@@ -58,7 +58,6 @@ export default function Hamburger() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="mb-[16px] justify-self-center min-[1024px]:hidden"
-                height="auto"
                 viewBox="0 -960 960 960"
                 width="40px"
                 fill="#e3e3e3"
