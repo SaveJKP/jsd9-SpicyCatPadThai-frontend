e665bfb1bde96aa9f5425feb1abf911115589547
Varinthorn <earnmaha@hotmail.com>
2025-05-01 22:09:58 +0700

    added password change feature

--- src/pages/UserSetting.jsx
+++ src/pages/UserSetting.jsx
@@ -21,6 +21,7 @@ import {
   TabsTrigger,
 } from "@/components/ui/tabs"
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 
 export default function UserProfile() {
@@ -36,6 +37,8 @@ export default function UserProfile() {
     city: "",
     country: "",
     phoneNumber: "",
+    currentPassword: "",
+    newPassword: "",
   });
 
   const [loading, setLoading] = useState(true); // For loading state
@@ -72,6 +75,7 @@ export default function UserProfile() {
   };
 
   //0.3 Handle form submission (send updated data to backend)
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsSubmitting(true); // Disable the button while submitting
@@ -95,6 +99,43 @@ export default function UserProfile() {
       setIsSubmitting(false); // Re-enable the button after submitting
     }
   };
+
+  //0.4handle password change
+  const navigate = useNavigate();
+  const handlePasswordChange = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const res = await axios.patch(
+        `http://localhost:3000/api/auth/user/${userId}/password`,
+        {
+          currentPassword: updatedUser.currentPassword,
+          newPassword: updatedUser.newPassword,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      alert("Password changed successfully!");
+      setUpdatedUser((prev) => ({ //copy everything from previous state such as name email to change only password. After copying and changing password,  reset password fields and  and 
+        ...prev,
+        currentPassword: "",
+        newPassword: "",
+      }));
+      navigate("/login");
+    } catch (error) {
+      const message =
+        error.response?.data?.message || "Failed to change password.";
+      alert(message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
 
   if (loading) {
     return <div>Loading...</div>;
@@ -125,123 +166,147 @@ export default function UserProfile() {
         </div>
 
         {/* 3.account and password setting - User Profile Setting*/}
-
+        
         <div className ="mt-4 px-16 font-noto md:w-1/2 md:px-8 lg:px-16">
-          <form onSubmit={handleSubmit}>
+          
             <Tabs defaultValue="account" className="">
               <TabsList className="grid w-full grid-cols-2  bg-(--color-box) mb-1">
                 <TabsTrigger value="account" className ="text-(--color-text)">Profile</TabsTrigger>
                 <TabsTrigger value="password" className ="text-(--color-text)">Password</TabsTrigger>
               </TabsList>
+              {/* 3.1.account setting */}
               <TabsContent  value="account" >
-                <Card>
-                  <CardHeader>
-                    <CardTitle className ="font-bold">Profile</CardTitle>
-                    <CardDescription>
-                      Make changes to your profile here. Click save when you're done.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent className="space-y-2">
-                    <div className="space-y-1">
-                      <Label htmlFor="name">Name</Label>
-                      <Input
-                        id="name"
-                        name="name"
-                        value={updatedUser.name}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="lastName">Last Name</Label>
-                      <Input
-                        id="lastName"
-                        name="lastName"
-                        value={updatedUser.lastName}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="email">Email</Label>
-                      <Input
-                        id="email"
-                        name="email"
-                        value={updatedUser.email}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="address">Address</Label>
-                      <Input
-                        id="address"
-                        name="address"
-                        value={updatedUser.address}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="city">City</Label>
-                      <Input
-                        id="city"
-                        name="city"
-                        value={updatedUser.city}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="country">Country</Label>
-                      <Input
-                        id="country"
-                        name="country"
-                        value={updatedUser.country}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="phoneNumber">Phone</Label>
-                      <Input
-                        id="phoneNumber"
-                        name="phoneNumber"
-                        value={updatedUser.phoneNumber}
-                        onChange={handleChange}
-                      />
-                    </div>
-                  </CardContent>
-                  <CardFooter>
-                    <Button
-                        type="submit"
-                        className="bg-(--color-greenBackground) hover:bg-[#060f0b] cursor-pointer font-bold"
-                        disabled={isSubmitting}
-                      >
-                        {isSubmitting ? "Saving..." : "Save Changes"}
-                    </Button>
-                  </CardFooter>
-                </Card>
+                <form onSubmit={handleSubmit}>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle className ="font-bold">Profile</CardTitle>
+                      <CardDescription>
+                        Make changes to your profile here. Click save when you're done.
+                      </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-2">
+                      <div className="space-y-1">
+                        <Label htmlFor="name">Name</Label>
+                        <Input
+                          id="name"
+                          name="name"
+                          value={updatedUser.name}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="lastName">Last Name</Label>
+                        <Input
+                          id="lastName"
+                          name="lastName"
+                          value={updatedUser.lastName}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                          id="email"
+                          name="email"
+                          value={updatedUser.email}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="address">Address</Label>
+                        <Input
+                          id="address"
+                          name="address"
+                          value={updatedUser.address}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="city">City</Label>
+                        <Input
+                          id="city"
+                          name="city"
+                          value={updatedUser.city}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="country">Country</Label>
+                        <Input
+                          id="country"
+                          name="country"
+                          value={updatedUser.country}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="phoneNumber">Phone</Label>
+                        <Input
+                          id="phoneNumber"
+                          name="phoneNumber"
+                          value={updatedUser.phoneNumber}
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </CardContent>
+                    <CardFooter>
+                      <Button
+                          type="submit"
+                          className="bg-(--color-greenBackground) hover:bg-[#060f0b] cursor-pointer font-bold"
+                          disabled={isSubmitting}
+                        >
+                          {isSubmitting ? "Saving..." : "Save Changes"}
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </form>
               </TabsContent>
+              
+              {/* 3.2.password setting */}
               <TabsContent value="password">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Password</CardTitle>
-                    <CardDescription>
-                      Change your password here. After saving, you'll be logged out.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent className="space-y-2">
-                    <div className="space-y-1">
-                      <Label htmlFor="current">Current password</Label>
-                      <Input id="current" type="password" />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="new">New password</Label>
-                      <Input id="new" type="password" />
-                    </div>
-                  </CardContent>
-                  <CardFooter>
-                    <Button className ="bg-(--color-greenBackground) hover:bg-[#060f0b] cursor-pointer font-bold">Save Password</Button>
-                  </CardFooter>
-                </Card>
+                <form onSubmit={handlePasswordChange}>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>Password</CardTitle>
+                      <CardDescription>
+                        Change your password here. After saving, you'll be logged out.
+                      </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-2">
+                      <div className="space-y-1">
+                        <Label htmlFor="current">Current password</Label>
+                        <Input
+                          id="current"
+                          type="password"
+                          name="currentPassword"
+                          value={updatedUser.currentPassword}
+                          onChange={handleChange} 
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <Label htmlFor="new">New password</Label>
+                        <Input
+                          id="new"
+                          type="password"
+                          name="newPassword"
+                          value={updatedUser.newPassword}
+                          onChange={handleChange} 
+                        />
+                      </div>
+                    </CardContent>
+                    <CardFooter>
+                    <Button
+                      type="submit"
+                      className="bg-(--color-greenBackground) hover:bg-[#060f0b] cursor-pointer font-bold"
+                      disabled={isSubmitting}
+                    >
+                      {isSubmitting ? "Saving..." : "Save Password"}
+                    </Button>
+                    </CardFooter>
+                  </Card>
+                </form>
               </TabsContent>
             </Tabs>
-          </form>
 
 
         </div>
