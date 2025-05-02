e75d72f3f067d597a1f08dacf854b2baa0591c2c
Varinthorn <earnmaha@hotmail.com>
2025-05-02 14:16:02 +0700

    Added Showsearch component and add showSearch component to the page search.jsx

---
+++ src/components/ShowSearch.jsx
@@ -0,0 +1,91 @@
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination";
+  import { BookCard } from "./BookCard";
+  import { useState } from "react";
+  import { bannersWithCategories } from "../data/ShowAll";
+  
+  export default function ShowSearch() {
+    {
+      /* set page */
+    }
+    const itemsPerPage = 8;
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = Math.ceil(bannersWithCategories.length / itemsPerPage);
+    const [open, setOpen] = useState(false);
+  
+    {
+      /* set index items to show book */
+    }
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentBanners = bannersWithCategories.slice(startIndex, endIndex);
+  
+    const handleNextPage = (e) => {
+      e.preventDefault();
+      if (currentPage < totalPages) {
+        setCurrentPage(currentPage + 1);
+      }
+    };
+  
+    const handlePreviousPage = (e) => {
+      e.preventDefault();
+      if (currentPage > 1) {
+        setCurrentPage(currentPage - 1);
+      }
+    };
+  
+    return (
+      <div>
+        <section className="flex flex-row flex-wrap items-center justify-center gap-4 p-8">
+          {currentBanners.map((banner) => (
+            <BookCard
+              key={banner.banner_id}
+              id={banner.banner_id}
+              title={banner.name}
+              banner={banner.picture}
+              author={banner.author_name}
+            />
+          ))}
+        </section>
+  
+        {totalPages > 1 && (
+          <Pagination className="p-8">
+            <PaginationContent>
+              <PaginationItem>
+                {/* Previous Page */}
+                <PaginationPrevious
+                  href="#"
+                  className="text-text text-xl"
+                  onClick={handlePreviousPage}
+                />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink href="#" className="text-banner text-xl" isActive>
+                  {currentPage}
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationEllipsis className="text-text text-xl" />
+              </PaginationItem>
+              <PaginationItem>
+                {/* Next Page */}
+                <PaginationNext
+                  href="#"
+                  className="text-text text-xl"
+                  onClick={handleNextPage}
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        )}
+      </div>
+    );
+  }
+  
