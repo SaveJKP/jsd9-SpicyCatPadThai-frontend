2d746d87e905032af8beb1b097ba8b071e304cb2
kkoryax <orachun.wsst@gmail.com>
2025-05-05 22:44:34 +0700

    hotfix(path): changed path image due to vercel not responde from mangadex path

--- src/data/ShowAll.js
+++ src/data/ShowAll.js
@@ -4,7 +4,7 @@ export const bannersWithCategories = [
       name: 'ยอดนักสืบจิ๋วโคนัน',
       author_name: 'Gosho Aoyama',
       catalog: 'โคนันฉบับรวมเล่ม',
-      picture: 'https://mangadex.org/covers/7f30dfc3-0b80-4dcc-a3b9-0cd746fac005/b65b80fa-217c-4a71-8e8f-5d647990117a.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/3/3f/Case_Closed_Volume_36.png',
       categories: [
         { category_id: 101, category_name: 'สืบสวน' },
         { category_id: 104, category_name: 'ตลก' }
@@ -15,7 +15,7 @@ export const bannersWithCategories = [
       name: 'วันพีซ',
       author_name: 'Eiichiro Oda',
       catalog: 'One Piece Tankobon',
-      picture: 'https://mangadex.org/covers/a1c7c817-4e59-43b7-9365-09675a149a6f/249fa95b-2214-4ae3-a8f7-77338fe34542.png',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg',
       categories: [
         { category_id: 102, category_name: 'ผจญภัย' },
         { category_id: 103, category_name: 'แอ็คชั่น' },
@@ -27,7 +27,7 @@ export const bannersWithCategories = [
       name: 'ดาบพิฆาตอสูร',
       author_name: 'Koyoharu Gotouge',
       catalog: 'Kimetsu no Yaiba Vol.',
-      picture: 'https://mangadex.org/covers/789642f8-ca89-4e4e-8f7b-eee4d17ea08b/60530e72-f76f-45d5-b6f9-f95e05058fc3.png',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Demon_Slayer_-_Kimetsu_no_Yaiba%2C_volume_1.jpg/250px-Demon_Slayer_-_Kimetsu_no_Yaiba%2C_volume_1.jpg',
       categories: [
         { category_id: 103, category_name: 'แอ็คชั่น' },
         { category_id: 107, category_name: 'แฟนตาซี' },
@@ -39,7 +39,7 @@ export const bannersWithCategories = [
       name: 'สปาย × แฟมิลี',
       author_name: 'Tatsuya Endo',
       catalog: 'SPY×FAMILY Comic',
-      picture: 'https://mangadex.org/covers/6b958848-c885-4735-9201-12ee77abcb3c/11c2207c-6886-43d2-90fd-c765127579c7.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Spy_Family_vol_1.jpg/250px-Spy_Family_vol_1.jpg',
       categories: [
         { category_id: 104, category_name: 'ตลก' },
         { category_id: 103, category_name: 'แอ็คชั่น' },
@@ -51,7 +51,7 @@ export const bannersWithCategories = [
       name: 'ผ่าพิภพไททัน',
       author_name: 'Hajime Isayama',
       catalog: 'Shingeki no Kyojin',
-      picture: 'https://mangadex.org/covers/304ceac3-8cdb-4fe7-acf7-2b6ff7a60613/628b5f74-4692-47d6-8fe3-75813714b433.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d6/Shingeki_no_Kyojin_manga_volume_1.jpg/250px-Shingeki_no_Kyojin_manga_volume_1.jpg',
       categories: [
         { category_id: 103, category_name: 'แอ็คชั่น' },
         { category_id: 106, category_name: 'ดราม่า' },
@@ -63,7 +63,7 @@ export const bannersWithCategories = [
       name: 'My Hero Academia',
       author_name: 'Kohei Horikoshi',
       catalog: 'Boku no Hero Academia',
-      picture: 'https://mangadex.org/covers/4f3bcae4-2d96-4c9d-932c-90181d9c873e/c7a7101a-8e22-442b-a1db-55ba9ef5b1ab.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/5a/Boku_no_Hero_Academia_Volume_1.png/250px-Boku_no_Hero_Academia_Volume_1.png',
       categories: [
         { category_id: 103, category_name: 'แอ็คชั่น' },
         { category_id: 110, category_name: 'พลังพิเศษ' },
@@ -75,7 +75,7 @@ export const bannersWithCategories = [
       name: 'Tokyo Revengers',
       author_name: 'Ken Wakui',
       catalog: 'Tokyo卍Revengers',
-      picture: 'https://mangadex.org/covers/59b36734-f2d6-46d7-97c0-06cfd2380852/3676571a-9d2b-48e6-8639-d4591725759a.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/b/b1/Tokyo_Revengers_volume_1_cover.jpg',
       categories: [
         { category_id: 103, category_name: 'แอ็คชั่น' },
         { category_id: 106, category_name: 'ดราม่า' },
@@ -87,7 +87,7 @@ export const bannersWithCategories = [
       name: 'Jujutsu Kaisen',
       author_name: 'Gege Akutami',
       catalog: 'Jujutsu Kaisen Vol.',
-      picture: 'https://mangadex.org/covers/c52b2ce3-7f95-469c-96b0-479524fb7a1a/7dc752c3-8c90-468e-8c75-6903e38d7c7f.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/4/46/Jujutsu_kaisen.jpg?20201222225027',
       categories: [
         { category_id: 103, category_name: 'แอ็คชั่น' },
         { category_id: 111, category_name: 'ไสยศาสตร์' },
@@ -99,7 +99,7 @@ export const bannersWithCategories = [
       name: 'เกิดใหม่ทั้งทีก็เป็นสไลม์ไปซะแล้ว',
       author_name: 'Fuse',
       catalog: 'Tensei Shitara Slime Datta Ken',
-      picture: 'https://mangadex.org/covers/e78a489b-6632-4d61-b00b-5206f5b8b22b/67de8b2f-c080-4006-91dd-a3b87abdb7fd.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/That_Time_I_Got_Reincarnated_as_a_Slime_light_novel_volume_1_cover.jpg/250px-That_Time_I_Got_Reincarnated_as_a_Slime_light_novel_volume_1_cover.jpg',
       categories: [
         { category_id: 107, category_name: 'แฟนตาซี' },
         { category_id: 102, category_name: 'ผจญภัย' }
@@ -110,7 +110,7 @@ export const bannersWithCategories = [
       name: 'Dr.STONE',
       author_name: 'Riichiro Inagaki',
       catalog: 'Dr.STONE Comic',
-      picture: 'https://mangadex.org/covers/89248e89-49cb-424b-899f-6c07f2289a96/15f7f116-bc9d-4214-a69b-98520055a045.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Doctor_stone.jpg/250px-Doctor_stone.jpg',
       categories: [
         { category_id: 102, category_name: 'ผจญภัย' },
         { category_id: 108, category_name: 'วิทยาศาสตร์' }
@@ -121,7 +121,7 @@ export const bannersWithCategories = [
       name: 'ชมรมคนไร้เพื่อน',
       author_name: 'Itachi',
       catalog: 'Boku wa Tomodachi ga Sukunai',
-      picture: 'https://mangadex.org/covers/30068342-34e3-43b1-a30f-75b5a485d004/594f83ef-c8e3-4314-8aa4-98cd208caf11.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/84/Haganai_LN_1.png/250px-Haganai_LN_1.png',
       categories: [
         { category_id: 104, category_name: 'ตลก' },
         { category_id: 109, category_name: 'ชีวิตในโรงเรียน' },
@@ -133,7 +133,7 @@ export const bannersWithCategories = [
       name: 'Bakuman.',
       author_name: 'Tsugumi Ohba',
       catalog: 'Bakuman. Comic',
-      picture: 'https://mangadex.org/covers/fa3e0b2f-4e1f-48ee-9af0-1de9dc28ca51/5ed89234-34ac-4bc9-8035-8e5d9ef14d13.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Bakuman_Vol_1_Cover.jpg/250px-Bakuman_Vol_1_Cover.jpg',
       categories: [
         { category_id: 109, category_name: 'ชีวิตในโรงเรียน' },
         { category_id: 106, category_name: 'ดราม่า' },
@@ -145,7 +145,7 @@ export const bannersWithCategories = [
       name: 'Kaguya-sama: Love Is War',
       author_name: 'Aka Akasaka',
       catalog: 'Kaguya-sama wa Kokurasetai',
-      picture: 'https://mangadex.org/covers/37f5cce0-8070-4ada-96e5-fa24b1bd4ff9/e21ca520-5054-4041-a07e-de8b7c683522.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/86/Kaguya-sama_-_Love_is_War%2C_volume_1.jpg/250px-Kaguya-sama_-_Love_is_War%2C_volume_1.jpg',
       categories: [
         { category_id: 104, category_name: 'ตลก' },
         { category_id: 105, category_name: 'โรแมนติก' },
@@ -157,7 +157,7 @@ export const bannersWithCategories = [
       name: 'Horimiya',
       author_name: 'Hero',
       catalog: 'Horimiya Comic',
-      picture: 'https://mangadex.org/covers/a25e46ec-30f7-4db6-89df-cacbc1d9a900/04feed00-d919-4c72-b355-15b37555eb1e.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/46/Hori-san_to_Miyamura-kun_volume_1_cover.jpg/250px-Hori-san_to_Miyamura-kun_volume_1_cover.jpg',
       categories: [
         { category_id: 105, category_name: 'โรแมนติก' },
         { category_id: 109, category_name: 'ชีวิตในโรงเรียน' }
@@ -168,7 +168,7 @@ export const bannersWithCategories = [
       name: 'Shokugeki no Soma (ยอดนักปรุงโซมะ)',
       author_name: 'Yuto Tsukuda',
       catalog: 'Shokugeki no Soma',
-      picture: 'https://mangadex.org/covers/5f20891f-0136-4fa8-afb7-d72f2af23c65/60aa7c6e-d714-468c-ad4a-043b820a6252.jpg',
+      picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Shokugeki_no_Souma_Volume_1.jpg/250px-Shokugeki_no_Souma_Volume_1.jpg',
       categories: [
         { category_id: 104, category_name: 'ตลก' },
         { category_id: 113, category_name: 'อาหาร' },
