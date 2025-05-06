e9b5b7362f4b72251401f41d4dca4f068f117353
atichapre <aticha.pre@gmail.com>
2025-05-07 01:11:33 +0700

    feat: adjusted order and added popover to navbar

---
+++ src/components/UserPopOver.jsx
@@ -0,0 +1,61 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const handleLogout = () => {
+  localStorage.removeItem("token");
+};
+
+// const [popOverOpen, setPopOverOpen] = useState(false);
+const togglePopOver = () => {
+  setPopOverOpen(!popOverOpen);
+};
+
+export function UserPopover() {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 -960 960 960"
+          className="hover:cursor-pointer"
+          width="75px"
+          fill="#e3e3e3"
+        >
+          <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
+        </svg>
+      </PopoverTrigger>
+      <PopoverContent className="w-80 bg-[var(--color-greenBackground)]">
+        <div className="flex w-[50%] flex-col gap-6 justify-self-center text-center text-[var(--color-text)]">
+          <Link className="leading-none font-medium hover:text-[var(--color-radio)]">
+            Username
+          </Link>
+          <Link className="leading-none font-medium hover:text-[var(--color-radio)]">
+            Orders
+          </Link>
+          <Link
+            to="/about"
+            className="leading-none font-medium hover:text-[var(--color-radio)]"
+            onClick={togglePopOver}
+          >
+            About Us
+          </Link>
+          <Link
+            to={"/login"}
+            onClick={() => {
+              handleLogout();
+              togglePopOver();
+            }}
+            className="my-2 flex justify-center rounded-xl bg-[var(--color-buttonBrown)] p-[8px] text-center text-[16px] hover:bg-[#bc7142cb]"
+          >
+            Log out
+          </Link>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -25,85 +25,92 @@ export default function OrderDetails() {
   );
 
   return (
-    <div className="container__div bg-[var(--color-text)] max-sm:rounded-t-2xl md:rounded-2xl">
-      <h2 className="py-[16px] pl-[16px] text-2xl font-bold">Order Details</h2>
-      <div className="flex w-full justify-self-center sm:max-md:flex-col">
-        <div className="w-[50%] flex-row sm:max-md:w-full">
-          <div className="flex-col border border-[var(--color-radio)]">
-            <div
-              key={orders.order_id}
-              className="mb-6 flex flex-col gap-1 p-[16px]"
-            >
-              <p className="flex justify-between font-semibold">
-                Order No: {orders.order_id}
-              </p>
+    <div className="bg-[var(--color-text)]">
+      <div className="container__div bg-[var(--color-text)]">
+        <h2 className="py-[16px] pl-[16px] text-2xl font-bold">
+          Order Details
+        </h2>
+        <div className="flex w-full justify-self-center sm:max-[815px]:flex-col">
+          <div className="w-[50%] flex-row sm:max-[815px]:w-full">
+            <div className="flex-col border border-[var(--color-radio)]">
+              <div
+                key={orders.order_id}
+                className="mb-6 flex flex-col gap-1 p-[16px]"
+              >
+                <p className="flex justify-between font-semibold">
+                  Order No: {orders.order_id}
+                </p>
 
-              <p className="text-sm text-gray-600">
-                Order Date:{" "}
-                {new Date(orders.created_at).toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                })}
-              </p>
-              <p className="pt-5 font-bold">Items Ordered</p>
+                <p className="text-sm text-gray-600">
+                  Order Date:{" "}
+                  {new Date(orders.created_at).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+                </p>
+                <p className="pt-5 font-bold">Items Ordered</p>
 
-              {orders.items.map((item) => (
-                <div key={item.name} className="mb-[16px] flex w-full flex-col">
-                  <div className="flex flex-row">
-                    <img
-                      src={
-                        item.picture ||
-                        "https://mir-s3-cdn-cf.behance.net/project_modules/1400/cdd17c167263253.6425cd49aab91.jpg"
-                      }
-                      className="max-h-[150px] max-w-[200px] object-contain object-top px-[8px]"
-                      alt={item.name}
-                    />
-                    <div className="flex w-full flex-col gap-1.5 px-[8px]">
-                      <h3 className="text-lg font-bold">{item.title}</h3>
-                      <h3 className="text-lg font-bold">{item.name}</h3>
-                      <p>{item.author}</p>
-                      <p>Vol. {item.vol_no}</p>
-                      <p>฿{item.price?.toFixed(2) || 0}</p>
+                {orders.items.map((item) => (
+                  <div
+                    key={item.name}
+                    className="mb-[16px] flex w-full flex-col"
+                  >
+                    <div className="flex flex-row">
+                      <img
+                        src={
+                          item.picture ||
+                          "https://mir-s3-cdn-cf.behance.net/project_modules/1400/cdd17c167263253.6425cd49aab91.jpg"
+                        }
+                        className="max-h-[150px] max-w-[200px] object-contain object-top px-[8px]"
+                        alt={item.name}
+                      />
+                      <div className="flex w-full flex-col gap-1.5 px-[8px]">
+                        <h3 className="text-lg font-bold">{item.title}</h3>
+                        <h3 className="text-lg font-bold">{item.name}</h3>
+                        <p>{item.author}</p>
+                        <p>Vol. {item.vol_no}</p>
+                        <p>฿{item.price?.toFixed(2) || 0}</p>
 
-                      <div className="flex flex-row justify-between py-4">
-                        <p className="text-base">Qty: {item.quantity}</p>
-                        <p className="text-base">
-                          Total price: ฿{item.price * item.quantity}
-                        </p>
+                        <div className="flex flex-row justify-between py-4">
+                          <p className="text-base">Qty: {item.quantity}</p>
+                          <p className="text-base">
+                            Total price: ฿{item.price * item.quantity}
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="w-[50%] border border-[var(--color-radio)] p-[16px] sm:max-md:w-full">
-          <div className="flex flex-row items-end justify-between pt-2 pb-9 text-2xl font-extrabold sm:max-md:my-5 sm:max-md:w-full">
-            <p className="font-semibold">Grand total:</p>
-            <p>฿{total.toFixed(2)}</p>
-          </div>
-          <p className="font-bold">Payment Information</p>
-          <div className="flex flex-col sm:max-md:flex-col">
-            <div className="sm:max-md:w-full">
-              <p className="pt-5 font-semibold">Billing Address</p>
-              "user Name"
-              <p>{orders.shipping_address.street}</p>
-              <p>{orders.shipping_address.city}</p>
-              <p>{orders.shipping_address.zip}</p>
+          <div className="w-[50%] border border-[var(--color-radio)] p-[16px] sm:max-[815px]:w-full">
+            <div className="flex flex-row items-end justify-between pt-2 pb-9 text-2xl font-extrabold sm:max-md:my-5 sm:max-md:w-full">
+              <p className="font-semibold">Grand total:</p>
+              <p>฿{total.toFixed(2)}</p>
+            </div>
+            <p className="font-bold">Payment Information</p>
+            <div className="flex flex-col sm:max-md:flex-col">
+              <div className="sm:max-md:w-full">
+                <p className="pt-5 font-semibold">Billing Address</p>
+                "user Name"
+                <p>{orders.shipping_address.street}</p>
+                <p>{orders.shipping_address.city}</p>
+                <p>{orders.shipping_address.zip}</p>
+              </div>
             </div>
           </div>
+          <button
+            className="mx-[58px] my-[32px] flex justify-center rounded-2xl bg-[var(--color-buttonBrown)] py-[10px] text-white hover:cursor-pointer"
+            onClick={() => {
+              navigate(`/orders/${orders.user_id}`);
+            }}
+          >
+            Back to Orders
+          </button>
         </div>
       </div>
-      <button
-        className="my-[64px] flex justify-self-center rounded-[10px] bg-[var(--color-buttonBrown)] px-[66px] py-[10px] text-white hover:cursor-pointer"
-        onClick={() => {
-          navigate(`/orders/${orders.user_id}`);
-        }}
-      >
-        Back to Orders
-      </button>
     </div>
   );
 }
