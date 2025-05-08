ba4789065a21d221fee0a18886955ebfff06ddb8
Varinthorn <earnmaha@hotmail.com>
2025-05-09 00:29:42 +0700

    fix: make search field clickable

--- src/components/ShowSearch.jsx
+++ src/components/ShowSearch.jsx
@@ -1,91 +1,112 @@
 import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-  } from "@/components/ui/pagination";
-  import { BookCard } from "./BookCard";
-  import { useState } from "react";
-  import { bannersWithCategories } from "../data/ShowAll";
-  
-  export default function ShowSearch() {
-    {
-      /* set page */
-    }
-    const itemsPerPage = 8;
-    const [currentPage, setCurrentPage] = useState(1);
-    const totalPages = Math.ceil(bannersWithCategories.length / itemsPerPage);
-    const [open, setOpen] = useState(false);
-  
-    {
-      /* set index items to show book */
-    }
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const currentBanners = bannersWithCategories.slice(startIndex, endIndex);
-  
-    const handleNextPage = (e) => {
-      e.preventDefault();
-      if (currentPage < totalPages) {
-        setCurrentPage(currentPage + 1);
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { BookCard } from "./BookCard";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+export default function ShowSearch() {
+  const [banners, setBanners] = useState([]);
+  {
+    /* set page */
+  }
+  const itemsPerPage = 12;
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(banners.length / itemsPerPage);
+
+  useEffect(() => {
+    const getItemsShowAll = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/titles/get-all",
+        );
+        setBanners(response.data.title);
+      } catch (err) {
+        console.log(err);
       }
     };
-  
-    const handlePreviousPage = (e) => {
-      e.preventDefault();
-      if (currentPage > 1) {
-        setCurrentPage(currentPage - 1);
-      }
-    };
-  
-    return (
-      <div>
-        <section className="flex flex-row flex-wrap items-center justify-center gap-4 p-8">
+
+    getItemsShowAll();
+  }, []);
+  {
+    /* set index items to show book */
+  }
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentBanners = banners.slice(startIndex, endIndex);
+
+  const handleNextPage = (e) => {
+    e.preventDefault();
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = (e) => {
+    e.preventDefault();
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  {
+    /* Get Items Banner */
+  }
+
+  return (
+    <div  className="container__div">
+      <div className="flex flex-col items-start justify-start">
+        <h2 className="px-[12%] text-white text-3xl md:text-4xl mt-12 font-semibold">📚 All Series</h2>
+        <section className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-8 justify-center items-center self-center">
           {currentBanners.map((banner) => (
             <BookCard
-              key={banner.banner_id}
-              id={banner.banner_id}
-              title={banner.name}
-              banner={banner.picture}
-              author={banner.author_name}
+              key={banner._id}
+              id={banner._id}
+              title={banner.title_name}
+              banner={banner.title_picture}
+              author={banner.author_id?.author_name}
             />
           ))}
         </section>
-  
-        {totalPages > 1 && (
-          <Pagination className="p-8">
-            <PaginationContent>
-              <PaginationItem>
-                {/* Previous Page */}
-                <PaginationPrevious
-                  href="#"
-                  className="text-text text-xl"
-                  onClick={handlePreviousPage}
-                />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#" className="text-banner text-xl" isActive>
-                  {currentPage}
-                </PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationEllipsis className="text-text text-xl" />
-              </PaginationItem>
-              <PaginationItem>
-                {/* Next Page */}
-                <PaginationNext
-                  href="#"
-                  className="text-text text-xl"
-                  onClick={handleNextPage}
-                />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
-        )}
       </div>
-    );
-  }
-  
+
+      {totalPages > 1 && (
+        <Pagination className="p-8">
+          <PaginationContent>
+            <PaginationItem>
+              {/* Previous Page */}
+              <PaginationPrevious
+                href="#"
+                className="text-text text-xl"
+                onClick={handlePreviousPage}
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" className="text-banner text-xl" isActive>
+                {currentPage}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              {/* <PaginationEllipsis className="text-text text-xl" /> */}
+            </PaginationItem>
+            <PaginationItem>
+              {/* Next Page */}
+              <PaginationNext
+                href="#"
+                className="text-text text-xl"
+                onClick={handleNextPage}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      )}
+    </div>
+  );
+}
