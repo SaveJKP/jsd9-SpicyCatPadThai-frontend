39af87fffe2cab8650dd944b5861c2d7e8c947a9
Varinthorn <earnmaha@hotmail.com>
2025-05-10 01:28:13 +0700

    refractor(navbar): handle search, search field, search icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
   const [orders, setOrders] = useState(null);
 
   const { totalQuantity } = useCart();
-  
+
   const [searchText, setSearchText] = useState('');
   const navigate = useNavigate();
   
@@ -27,10 +27,10 @@ export const Navbar = () => {
     }, 1000);
   };
 
-  const handleSearch = (e) => {
-    if (e.key === 'Enter' && searchText.trim() !== '') {
-      e.preventDefault()
-      navigate(`/search?query=${encodeURIComponent(searchText.trim())}`);
+  const handleSearch = () => {
+    const textTrimmed = searchText.trim();
+    if (textTrimmed !== "") {
+      navigate(`/search?query=${encodeURIComponent(textTrimmed)}`);
     }
   };
 
@@ -87,7 +87,12 @@ export const Navbar = () => {
                 onClick={() => navigate('/search')}
                 value={searchText}
                 onChange={(e) => setSearchText(e.target.value)}
-                onKeyDown={handleSearch}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    e.preventDefault();
+                    handleSearch();
+                  }
+                }}
               />
               {/* search icon */}
               <Link to={"/search"}>
@@ -98,9 +103,9 @@ export const Navbar = () => {
                   width="32px"
                   fill="#d9d9d9"
                   onClick={(e) => {
-                  e.preventDefault();
-                  navigate(`/search?query=${encodeURIComponent(searchText.trim())}`);
-                }}
+                    e.preventDefault();
+                    handleSearchNavigation();
+                  }}
                 >
                   <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
                 </svg>
