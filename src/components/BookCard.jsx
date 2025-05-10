c27c23bed62f948b5c5d187a2728e65be284c448
kkoryax <orachun.wsst@gmail.com>
2025-05-10 16:33:14 +0700

    feat(component): updated logic for rendering image on Book Card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -12,7 +12,6 @@ import { createPortal } from "react-dom";
 
 export function BookCard({ id,title, banner, author }) {
   const [open, setOpen] = useState(false);
-  console.log(banner, title)
 
   const handleOpenModal = () => {
     setOpen(true);
@@ -21,7 +20,7 @@ export function BookCard({ id,title, banner, author }) {
     <>
       <div>
         <Card onClick={handleOpenModal} className="bg-box border-banner flex h-75 w-45 flex-col items-center justify-center hover:cursor-pointer">
-          <img src={ `${banner}` || "https://placehold.co/200x250"} alt={title} className="w-full aspect-[3/4] object-cover -mb-8 p-4" />
+          <img src={ banner || "https://placehold.co/200x250"} alt={title} className="w-full aspect-[3/4] object-cover -mb-8 p-4" />
           <CardContent>
             <CardTitle className="text-text text-lg font-bold line-clamp-1">
               {title}
