1e8c6cdcd5a6f681c349176bcbbb615698fb3fda
atichapre <aticha.pre@gmail.com>
2025-05-11 15:35:53 +0700

    fixed conflicts

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,95 +1,120 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { Link, useNavigate } from "react-router-dom";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
 import { useAuth } from "../context/userContext";
 import axios from "axios";
 
 export default function Login() {
   const { setUser } = useAuth();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const [ error, setError ] = useState("")
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = async(e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     setError("");
 
     try {
-      const response = await axios.post('http://localhost:3000/api/auth/login', {
-        email,
-        password
-        }, {
-        withCredentials: true
-      }
-    );
-    console.log(response.data.user)
-    setUser(response.data.user);
-    navigate("/")
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      console.log(response);
+      setUser(response.data);
+      navigate("/");
     } catch (err) {
       console.error(err);
       setError(
-        err?.response?.data?.message || "Login failed. Please try again."
-      )
+        err?.response?.data?.message || "Login failed. Please try again.",
+      );
     }
-  }
+  };
 
   return (
-  <main >
-    <div className="bg-greenBackground w-full h-fit">
-      <div className="container__div">
-        <section className="flex flex-col mt-[10%] mb-[5%] justify-center lg:flex-row items-center w-full h-full gap-4">
+    <main>
+      <div className="bg-greenBackground h-fit w-full">
+        <div className="container__div">
+          <section className="mt-[10%] mb-[5%] flex h-full w-full flex-col items-center justify-center gap-4 lg:flex-row">
+            {/* Icon Section */}
+            <section className="text-text flex w-full flex-col items-center justify-center gap-4 lg:w-1/2">
+              <img
+                src="/logo_katsubook.png"
+                alt="Katsu Bookstore"
+                className="hidden w-[50%] lg:flex"
+              />
+              <img
+                src="/logo_katsubook_no-text.png"
+                alt="Katsu Bookstore"
+                className="flex w-[35%] md:w-[15%] lg:hidden"
+              />
+              <p className="hidden w-fit lg:flex">
+                Welcome to bookstore Everything in One place
+              </p>
+            </section>
+            <section className="text-text flex w-full flex-col items-center justify-center gap-4 lg:w-1/2">
+              <h1 className="flex font-bold">Login</h1>
+              <p className="hidden md:flex lg:hidden">
+                Welcome to bookstore Everything in One place
+              </p>
 
-          {/* Icon Section */}
-          <section className="flex flex-col justify-center items-center gap-4 w-full lg:w-1/2 text-text">
-            <img src="/logo_katsubook.png" alt="Katsu Bookstore"
-            className="w-[50%] hidden lg:flex"/>
-            <img src="/logo_katsubook_no-text.png" alt="Katsu Bookstore"
-            className="w-[35%] md:w-[15%] flex lg:hidden"/>
-            <p className="hidden lg:flex w-fit">Welcome to bookstore Everything in One place</p>
-          </section>
-          <section className="flex flex-col justify-center items-center gap-4 w-full lg:w-1/2 text-text">
-            <h1 className="flex font-bold">Login</h1>
-            <p className="hidden md:flex lg:hidden">Welcome to bookstore Everything in One place</p>
-
-            {error && (
-              <div className="bg-red-100 text-red-700 px-4 py-2 rounded mb-4 text-center">
+              {error && (
+                <div className="mb-4 rounded bg-red-100 px-4 py-2 text-center text-red-700">
                   {error}
-              </div>
-             )}
+                </div>
+              )}
 
-            {/* Sign In Form */}
-            <form onSubmit={handleLogin} className="flex flex-col justify-center items-center gap-4 w-full">
-              <input type="email" placeholder="Email" className="w-[65%] md:w-[35%] lg:w-[50%] bg-white text-banner px-4 py-2 rounded-2xl"
-              value={email} onChange={(e) => setEmail(e.target.value)}/>
+              {/* Sign In Form */}
+              <form
+                onSubmit={handleLogin}
+                className="flex w-full flex-col items-center justify-center gap-4"
+              >
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="text-banner w-[65%] rounded-2xl bg-white px-4 py-2 md:w-[35%] lg:w-[50%]"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
 
-              <input type="password" placeholder="Password" className="w-[65%] md:w-[35%] lg:w-[50%] bg-white text-banner px-4 py-2 rounded-2xl"
-              value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="text-banner w-[65%] rounded-2xl bg-white px-4 py-2 md:w-[35%] lg:w-[50%]"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
 
-              <Link to="/forgot-password">Forget Your password?</Link>
-              <div className="flex flex-col justify-center items-center gap-4 w-[65%] md:w-[35%] lg:w-[50%]">
-                <Separator />
-                <Button type="submit" className="w-full bg-buttonBrown px-4 py-2 font-semibold hover:cursor-pointer rounded-2xl">Login</Button>
-                <p>or</p>
-
-                {/* Register Button */}
-                <Link to="/register" className="w-full">
-                  <Button className="w-full bg-buttonBlue px-4 py-2 font-semibold hover:cursor-pointer rounded-2xl">
-                    New Account
+                <Link to="/forgot-password">Forget Your password?</Link>
+                <div className="flex w-[65%] flex-col items-center justify-center gap-4 md:w-[35%] lg:w-[50%]">
+                  <Separator />
+                  <Button
+                    type="submit"
+                    className="bg-buttonBrown w-full rounded-2xl px-4 py-2 font-semibold hover:cursor-pointer"
+                  >
+                    Login
                   </Button>
-                </Link>
+                  <p>or</p>
 
-              </div>
-            </form>
-
+                  {/* Register Button */}
+                  <Link to="/register" className="w-full">
+                    <Button className="bg-buttonBlue w-full rounded-2xl px-4 py-2 font-semibold hover:cursor-pointer">
+                      New Account
+                    </Button>
+                  </Link>
+                </div>
+              </form>
+            </section>
           </section>
-        </section>
+        </div>
       </div>
-    </div>
-  </main>
-  )
+    </main>
+  );
 }
